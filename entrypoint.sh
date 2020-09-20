49e160b630ef02fb21191f40340700b65d7d0c4c
phenry20 <56044757+phenry20@users.noreply.github.com>
2020-09-19 21:43:32 -0400

    Update entrypoint.sh
    Made push optional with PUSH flag

--- entrypoint.sh
+++ entrypoint.sh
@@ -19,7 +19,10 @@ fi
 gcloud auth activate-service-account --key-file="$HOME"/gcloud.json --project "$INPUT_PROJECT"
 gcloud auth configure-docker
 
-docker push "$INPUT_IMAGE"
+if [ "$PUSH" ]
+then
+    docker push "$INPUT_IMAGE"
+fi
 
 gcloud beta run deploy "$INPUT_SERVICE" \
   --image "$INPUT_IMAGE" \
